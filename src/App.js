e8da272e8c2174a6b1d61639273f9727be7b941b
d4nQw3rty <danlinmoy@gmail.com>
2022-11-02 09:43:40 -0300

    set routes
    -set routes in app.js
    -create navbar.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Home from './components/Home';
 
-class App extends React.PureComponent {
-  render() {
-    return (
-      <div className="App">
-        <section>
-          <Calculator />
-          <Home />
-        </section>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router className="App">
+    <Navbar />
+    <Routes>
+      <Route path="Calculator" element={<Calculator />} />
+      <Route path="Home" element={<Home />} />
+      <Route path="Quote" element={<Quote />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
