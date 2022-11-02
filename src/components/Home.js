0e35daf7670b6c607b6e97773501dabd2784c95a
d4nQw3rty <danlinmoy@gmail.com>
2022-11-02 09:18:44 -0300

    create home component ans style

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+      <h2>Welcome to our page!</h2>
+      <p className={styles.p1}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Animi cumque sit quaerat cum,ratione soluta! Eveniet, magnam
+        eum ea excepturi et temporibus laborum at officia!
+      </p>
+      <hr />
+      <p className={styles.p2}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elite.
+        Animi cumque sit quaerat cum, ratione soluta! Eveniet, magnam
+        eum ea excepturi et temporibus laborum at officia!
+      </p>
+    </div>
+  );
+  
+  export default Home;
