671dbf81bf453d38340958b904f4123ad70a36f2
d4nQw3rty <danlinmoy@gmail.com>
2022-11-02 11:35:29 -0300

    Style home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,27 @@ import styles from './Home.module.css';
 const Home = () => (
   <div>
     <h2>Welcome to our page!</h2>
-    <p className={styles.p1}>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-      Animi cumque sit quaerat cum,ratione soluta! Eveniet, magnam
-      eum ea excepturi et temporibus laborum at officia!
-    </p>
-    <hr />
-    <p className={styles.p2}>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elite.
-      Animi cumque sit quaerat cum, ratione soluta! Eveniet, magnam
-      eum ea excepturi et temporibus laborum at officia!
-    </p>
+    <div className={styles.home}>
+      <p className={styles.p1}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+        tenetur error, harum nesciunt ipsum debitis quas aliquid.
+      </p>
+
+      <p className={styles.p2}>
+        Lorem ipsum dolor sit amet, consectetueradipiscing elit. Aenean commodo ligula
+        eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes
+        , nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
+        pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
+        vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
+        venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
+        tincidunt.
+      </p>
+    </div>
   </div>
 );
 
