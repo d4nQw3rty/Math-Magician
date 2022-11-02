ca245c4707a23cc97a4e2c1455e9ad04f57f7719
d4nQw3rty <danlinmoy@gmail.com>
2022-11-02 13:37:12 -0300

    Fix linters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,13 @@ const links = [
     id: 1,
     path: '/Home',
     text: 'Home',
+    separator: '|',
   },
   {
     id: 2,
     path: '/calculator',
     text: 'Calculator',
+    separator: '|',
   },
   {
     id: 3,
@@ -20,20 +22,26 @@ const links = [
 ];
 
 const Navbar = () => (
-  <header>
-    <h1>Math Magicians</h1>
-    <nav>
-      <ul className="list">
-        {links.map((link) => (
-          <li key={link.id}>
-            <NavLink to={link.path} activeClassName="active-link" className="link" exact>
-              {link.text}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  </header>
+  <>
+    <header>
+      <h1>Math Magicians</h1>
+      <nav>
+        <ul className="list">
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink to={link.path} activeClassName="active-link" className="link" exact>
+                { link.text }
+              </NavLink>
+              <span>
+                {link.separator}
+              </span>
+            </li>
+          ))}
+
+        </ul>
+      </nav>
+    </header>
+  </>
 );
 
 export default Navbar;
