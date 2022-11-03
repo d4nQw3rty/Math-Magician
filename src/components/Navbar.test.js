21cab8a0ea055f353ad616ae178f7fa97e1187f1
d4nQw3rty <danlinmoy@gmail.com>
2022-11-03 09:02:54 -0300

    Update Navbar.test.js
    Fix linter

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
 describe('testing Navbar component', () => {
   test('Navbar renders correctly', () => {
     const tree = renderer.create(
-    <Router>
-      <Navbar/>
-    </Router>).toJSON();
+      <Router>
+        <Navbar />
+      </Router>,
+    ).toJSON();
     expect(tree).toMatchSnapshot();
-  })
-})
+  });
+});
