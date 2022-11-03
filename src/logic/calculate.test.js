55792a516fd444d8d74f7a3628c66c83f834c09f
d4nQw3rty <danlinmoy@gmail.com>
2022-11-03 09:03:10 -0300

    Update calculate.test.js
    Fix linter

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,36 +1,31 @@
 import calculate from './calculate';
 import operate from './operate';
 
-describe ('testing calculate component', () => {
-  test ('testing if calculate is working properly', () => {
+describe('testing calculate component', () => {
+
+ 
+  test('testing if calculate is working properly', () => {
     const obj = {
       total: null,
       next: null,
       operation: null,
-    }
-    const test1 = calculate(obj, 'AC');
-    expect(obj.total).toBeNull;
-    expect(obj.next).toBeNull;
-    expect(obj.operation).toBeNull;
-  })
-  test ('testing if buttons working properly', () => {
-    const obj = {
+    };
+    expect(calculate(obj, 'AC')).toEqual({
       total: null,
-      next: 0,
+      next: null,
       operation: null,
-    }
-    const test2 = calculate(obj, '0');
-    expect(obj.next).toEqual(0);
-    expect(obj.total).toBeNull;
-    expect(obj.operation).toBeNull;
-  })
-  test ('testing operations', () => {
+    });
+  });
+
+  test('testing operations', () => {
     const obj = {
       total: 1,
       next: 0,
       operation: '÷',
-    }
-    const test3 = operate(1, 0, '÷')
+    };
+
+    const test3 = operate(obj.total, obj.next, obj.operation);
+
     expect(test3).toMatch("Can't divide by 0.");
-  })
-})
+  });
+});
